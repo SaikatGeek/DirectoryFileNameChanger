9b0721eabf7fec97dbf3916c3c9429e581654bbe
SaikatGeek <saikat.geek@gmail.com>
2022-04-03 22:05:19 +0600

    Index file added

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$dir = new \DirectoryIterator( realpath( "" ) );
+
+
+foreach ($dir as $fileInfo) {
+    if( $fileName = str_replace( "replace", "replace_by", $filename ) ) {
+
+        rename(
+            "old name" . $fileInfo->getFilename(), 
+            "new_name" . $fileName
+        );
+    }
+}
+
+
